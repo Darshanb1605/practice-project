9ab7e3345057e78475bcb51ed56a9cb687219806
darshan <darshanb1693@gmail.com>
2024-11-27 00:05:54 +0530

    schell script edited

--- odd.sh
+++ odd.sh
@@ -3,7 +3,7 @@ echo "enter the number"
 read number
 if [ $((number % 2)) -eq 0 ]
 then
-echo "$number is even"
+echo "$number is even number"
 else
-echo "$number is odd"
+echo "$number is odd number"
 fi
